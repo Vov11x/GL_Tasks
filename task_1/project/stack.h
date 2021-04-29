699ca90ab70d0d9f15519c127d33f1da65d78315
Vova Makotkin <66742594+Vov11x@users.noreply.github.com>
2021-04-29 20:30:11 +0300

    Update: stack.h

--- task_1/project/stack.h
+++ task_1/project/stack.h
@@ -1,18 +1,21 @@
-#include <forward_list>
+#include <vector>
 
 
 template <typename T>
 class stack {
-    std::forward_list<T> fList; // main container
-    size_t max_size_{};     // size of stack
-    size_t current_size_{}; // capacity
 public:
-    stack() = default;
-    explicit stack(size_t size) { create(size); }
-    void create(size_t size) noexcept;
+    stack()                     : m_max_size{0}, m_current_size{0} {}
+    explicit stack(size_t size) : m_max_size{0}, m_current_size{0}  { create(size); }
+    
+    void create(size_t size);
     void push(const T& data);
     T pop();
-    [[nodiscard]] size_t capacity() const noexcept { return current_size_; }
-    [[nodiscard]] size_t size() const noexcept { return max_size_; }
-    void resize(size_t new_size) noexcept;
+    [[nodiscard]] size_t capacity() const { return m_max_size; }
+    [[nodiscard]] size_t size()     const { return m_current_size; }
+    void resize(size_t new_size);
+
+private:
+    std::vector<T> m_data;  // vector stack based on
+    size_t m_max_size;      // maximum amount of elements
+    size_t m_current_size;  // amount of elements in stack
 };
