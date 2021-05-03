db54e0ac3bb4762bc6bd13723e38a655981727e0
Vova Makotkin <66742594+Vov11x@users.noreply.github.com>
2021-05-03 21:59:18 +0300

    Update: stack header

--- task_1/project/stack.h
+++ task_1/project/stack.h
@@ -4,18 +4,14 @@
 template <typename T>
 class stack {
 public:
-    stack()                     : m_max_size{0}, m_current_size{0} {}
-    explicit stack(size_t size) : m_max_size{0}, m_current_size{0}  { create(size); }
-    
-    void create(size_t size);
+    explicit stack(size_t size);
+
     void push(const T& data);
     T pop();
-    [[nodiscard]] size_t capacity() const { return m_max_size; }
-    [[nodiscard]] size_t size()     const { return m_current_size; }
+    [[nodiscard]] size_t capacity() const { return m_data.capacity(); }
+    [[nodiscard]] size_t size()     const { return m_data.size(); }
     void resize(size_t new_size);
 
 private:
-    std::vector<T> m_data;  // vector stack based on
-    size_t m_max_size;      // maximum amount of elements
-    size_t m_current_size;  // amount of elements in stack
+    std::vector<T> m_data;
 };
